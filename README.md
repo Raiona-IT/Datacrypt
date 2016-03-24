e5b90fb42b73417467fb7b594d5a17ba86290d9d
KostasMp <mpenos.ks@gmail.com>
2016-03-25 01:03:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Datacrypt
+A simple python script used to hide file(s) into an image.   
+
+**Usage:** 
+```sh
+datacrypt.py IMG [-h] ( -d | -e FILE [FILE...])
+```
+
+### To-do's
+    - Add hidden file export
+    - Add automated check, before writing new image, for files with the same name to avoid overwriting
+    - Add option for specific output image name
+    - Add option for file compresion before encrypting them in image
+    - Add option to automatically convert any non-png image to png, if possible
+
+License
+---
+The MIT License (MIT)
---
+++ datacrypt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Hide file(s) in an image')
+mutex = parser.add_mutually_exclusive_group(required=True)
+parser.add_argument('image', metavar='IMG', help='The image where the file(s) will be hidden')
+mutex.add_argument('-d', '--decrypt', action='store_true', help='Extract any hidden files from the specified image')
+mutex.add_argument('-e', '--encrypt', metavar='FILE', nargs='+', help='The file(s) to be hidden')
+args = parser.parse_args()
+
+if args.encrypt:
+    files = ''
+    for item in args.encrypt:
+        files = files + ' ' + item
+    outfile = 'crypt-' + args.image
+    print files
+    os.system('cat ' + args.image + ' ' + files + ' > ' + outfile)
+elif args.decrypt:
+    print args.decrypt
